8fa4ef2c0e4edd91a95a0eb451898e989b36392f
krisaramis <kristina.kulitsch@gmail.com>
2020-06-08 21:00:05 +0300

    added LettersCount method

---
+++ Practice/GoodDay/Greeting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoodDay
+{
+    class Greeting
+    {
+        DateTime currentdate = DateTime Now { get; };
+
+        //
+        // Summary:
+        //     Gets a System.DateTime object that is set to the current date and time on this
+        //     computer, expressed as the local time.
+        //
+        // Returns:
+        //     An object whose value is the current local date and time.
+        public static DateTime Now { get; }
+
+
+
+    }
+}
