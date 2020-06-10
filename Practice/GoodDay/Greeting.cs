d20d95c0c5f796d5e57288fccee6d5e238063a39
krisaramis <kristina.kulitsch@gmail.com>
2020-06-10 21:31:39 +0300

    lecture 6 task ans files from practice 1 after conflicts

---
+++ Practice/GoodDay/Greeting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+ using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Collections.ObjectModel;
+
+namespace GoodDay
+{
+    class Greeting
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentdate = DateTime.Now; // год - месяц - день
+            DateTime date2 = new DateTime(2010, 8, 18, 13, 30, 30);
+            TimeSpan number = TimeSpan.FromHours(9);
+            Console.WriteLine(number);
+           TimeSpan fierstinerval = new Hours
+
+        }
+        public int Hours { get; }
+            public TimeSpan TimeOfDay { get; }
+
+        
+
+    }
+}
+
---
+++ lecture.6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Serialization;
+
+namespace lecture._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              void Task1()
+            {
+                int[] empty_massive = new int[0];
+             }
+
+            void Task2()
+            {
+                //int a = 32; char b = 'A'; string c = "Hello";
+                //int [] massive_of_3 = new[] {a,b,c};
+                //massive_of_3[0]= 
+            }
+            void Task3()
+            {
+                int[] massive_random = new int[12];
+                Random R = new Random();
+                for (int i = 0; i <= 12; i++)
+                    massive_random[i] = R.Next();
+                Console.WriteLine($"massive:{massive_random}");
+            }
+        }
+    }
+}
