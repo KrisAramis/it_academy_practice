69911696cedafb5ab55ae9ecb94686e83802902a
krisaramis <kristina.kulitsch@gmail.com>
2020-06-13 09:15:31 +0300

    added methods for each task and invocated them in Main method

--- lecture.6/Program.cs
+++ lecture.6/Program.cs
@@ -8,9 +8,13 @@ namespace lecture._6
 {
     class Program
     {
-        static void Main(string[] args)
+        public void Emptyarray()
         {
             int[] empty_massive = new int[0];
+        }
+
+        public void Diffarray()
+        {
 
             Object[] massive_of_3 = { 32, 'A', "Hello" };
             foreach (object i in massive_of_3)
@@ -23,11 +27,10 @@ namespace lecture._6
             Console.WriteLine(massive_of_3[0]);
             massive_of_3[massive_of_3.Length - 1] = massive_of_3[massive_of_3.Length - 1] + ", guys";
             Console.WriteLine(massive_of_3[massive_of_3.Length - 1]);
+        }
 
-
-
-
-
+        public void Randomarray()
+        {
             int[] massive_random = new int[12];
             Random R = new Random();
             for (int i = 0; i < 12; i++)
@@ -36,10 +39,26 @@ namespace lecture._6
                 Console.WriteLine($"massive number {i}:{massive_random[i]}");
                 Console.WriteLine($"the maxvalue of random massive is:{massive_random.Max()}");
             }
+        }
 
+        public void BoxUnbox()
+        {
             short variable = 4040;
             object obj = variable;
-            sbyte unbox_obj = (sbyte) obj;
+            sbyte unbox_obj = (sbyte) (short)obj;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program task1 = new Program();
+            task1.Emptyarray();
+            Program task2 = new Program();
+            task2.Diffarray();
+            Program task3 = new Program();
+            task3.Randomarray();
+            Program task4 = new Program();
+            task4.BoxUnbox();
         }
     }
 }
