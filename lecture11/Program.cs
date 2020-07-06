64c39b150f3d5bffbd85fabd376ab1fe7cb6278d
krisaramis <kristina.kulitsch@gmail.com>
2020-07-06 20:11:28 +0300

    added task

---
+++ lecture11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lecture11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UniqueItem tom = new UniqueItem();
+            tom.getID();
+            Console.WriteLine(tom.getID());
+            UniqueItem bob = new UniqueItem();
+            bob.getID();
+            Console.WriteLine(bob.getID());
+
+
+        }
+    }
+}
