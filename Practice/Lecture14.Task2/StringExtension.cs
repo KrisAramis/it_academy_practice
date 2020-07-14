4f3fccc2e191816694af405d8130173b215908e6
krisaramis <kristina.kulitsch@gmail.com>
2020-07-14 16:10:48 +0300

    added 2nd task

---
+++ Practice/Lecture14.Task2/StringExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Lecture14.Task2
+{
+    public static class StringExtension
+    {
+        public static string  CutString ( this string str)
+        {
+            const int startsymbol = 0;
+            const int numberofsymbols = 5;
+            string newstr = str.Substring(startsymbol, numberofsymbols);
+            const string threepoints = "...";
+            string cutstring = String.Concat(newstr, threepoints);
+            return cutstring;
+        }
+    }
+}
