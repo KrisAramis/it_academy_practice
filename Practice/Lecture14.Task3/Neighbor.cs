6997431b65ea896fb272d62e515f1e08511fd83d
krisaramis <kristina.kulitsch@gmail.com>
2020-07-14 17:48:14 +0300

    will add the complete solution later

---
+++ Practice/Lecture14.Task3/Neighbor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture14.Task3
+{
+    public class Neighbors
+    {
+        public string Fullname { get; set; }
+        public int Flatnumber { get; set; }
+        public short PhoneNumber { get; set; }
+
+    }
+
+    
+}
