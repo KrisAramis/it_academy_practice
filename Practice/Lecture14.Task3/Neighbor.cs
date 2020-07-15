95b62859ab077a2cedd2e9350fd3335cb086797a
krisaramis <kristina.kulitsch@gmail.com>
2020-07-15 10:45:46 +0300

    added 3rd task

--- Practice/Lecture14.Task3/Neighbor.cs
+++ Practice/Lecture14.Task3/Neighbor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Lecture14.Task3
@@ -9,8 +10,23 @@ namespace Lecture14.Task3
         public string Fullname { get; set; }
         public int Flatnumber { get; set; }
         public short PhoneNumber { get; set; }
-
+       
+             public static void ReturnNeighborbyFlatNo(IList<Neighbors> floorNeighbors)
+            {
+                Console.WriteLine("Add number of the flat");
+                string flatNo = Console.ReadLine();
+                foreach (Neighbors item in floorNeighbors)
+                {
+                    if (item.Flatnumber.Equals(Convert.ToInt32(flatNo)))
+                    {
+                        Console.WriteLine($"Fullname: {item.Fullname}, {item.PhoneNumber}");
+                    }
+                }
+             }
     }
+}
 
     
-}
+
+    
+
--- Practice/Lecture14.Task3/Program.cs
+++ Practice/Lecture14.Task3/Program.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 
 namespace Lecture14.Task3
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            List <Neighbors> floorNeighbors = new List <Neighbors>(10);
+            List<Neighbors> floorNeighbors = new List<Neighbors>(10);
             floorNeighbors.Add(new Neighbors() { Fullname = "John Dohry", Flatnumber = 43, PhoneNumber = 444 - 333 });
             floorNeighbors.Add(new Neighbors() { Fullname = "Lewis Hamilton", Flatnumber = 32, PhoneNumber = 220 - 333 });
             floorNeighbors.Add(new Neighbors() { Fullname = "Kristin Hamilton", Flatnumber = 21, PhoneNumber = 342 - 342 });
 
-            floorNeighbors.
-
-        }
+            Neighbors.ReturnNeighborbyFlatNo(floorNeighbors);
+         }
     }
 }
